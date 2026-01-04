d1c8995135a4fcfc2770d29985c0e3131902eab0
bobllor <bobllor@yahoo.com>
2026-01-04 03:43:07 -0600

    added AppLauncher

--- app.ts
+++ app.ts
@@ -1,10 +1,14 @@
 import app from "ags/gtk4/app"
 import style from "./style.scss"
 import Bar from "./widget/Bar"
+import AppLauncher from "./widget/AppLauncher/AppLauncher"
 
 app.start({
   css: style,
   main() {
-    app.get_monitors().map(Bar)
+    app.get_monitors().map(monitor => {
+      Bar(monitor);
+      AppLauncher({gdkmonitor: monitor});
+    })
   },
 })
