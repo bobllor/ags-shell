ffe66b521a5416a95b0acbd44640ba288fba9358
bobllor <bobllor@yahoo.com>
2026-01-05 17:39:19 -0600

    updated onKey to handle searching and starting applications, added click handling for closing launcher, updated code to use states for Gtk objects, and display all applications by default

--- widget/AppLauncher.tsx
+++ widget/AppLauncher.tsx
@@ -3,6 +3,8 @@ import { createEffect, createState, State } from "gnim"
 import { Search } from "./AppLauncher/AppBox/Search";
 import AstalApps from "gi://AstalApps?version=0.1";
 import AppList from "./AppLauncher/AppBox/AppList";
+import Graphene from "gi://Graphene?version=1.0";
+import { getKey } from "../utils";
 
 export default function AppLauncher({gdkmonitor, visible = true}: Props): JSX.Element{
     const astalApp: AstalApps.Apps = new AstalApps.Apps();
@@ -12,44 +14,86 @@ export default function AppLauncher({gdkmonitor, visible = true}: Props): JSX.El
 
     createEffect(() => {
         const foundApplications: AstalApps.Application[] = astalApp.exact_query(textVal());
-
+        
         setApplications(() => {
-            if(foundApplications.length > 8){
-                return [...foundApplications].slice(0, 8);
-            }
-            
             return [...foundApplications];
         });
     })
 
-
-    let searchEntry: Gtk.Entry | undefined = undefined;
-    let appWindow: Gtk.Window;
+    const [appWindow, setAppWindow] = createState(new Gtk.Window);
+    const [searchEntry, setSearchEntry] = createState(new Gtk.Entry);
+    const [appBox, setAppBox] = createState(new Gtk.Box);
 
     return (
         <window
         class="AppLauncher"
-        $={(ref) => (appWindow = ref)}
+        $={(ref) => setAppWindow(ref)}
         visible={visible}
         exclusivity={Astal.Exclusivity.IGNORE}
         keymode={Astal.Keymode.EXCLUSIVE}
         gdkmonitor={gdkmonitor}>
-            <Gtk.EventControllerKey onKeyPressed={(_e, keyVal, _, _mod) => onKey(keyVal, appWindow)}/>
-            <Gtk.GestureClick onPressed={(_e, _, x, y) => {console.log(x, y)}} />
+            <Gtk.EventControllerKey onKeyPressed={(_e, keyVal, _, mod) => onKey(keyVal, mod, appWindow(), searchEntry(), applications())}/>
+            <Gtk.GestureClick onPressed={(_e, _, x, y) => handleClick(x, y, appBox(), appWindow())} />
             <box 
+            $={(ref) => setAppBox(ref)}
             orientation={Gtk.Orientation.VERTICAL}
             class="app-box">
-                <Search searchEntry={searchEntry} setTextVal={setTextVal} />
-                <AppList applications={applications} />
+                <Search setEntry={setSearchEntry} setTextVal={setTextVal}/>
+                <AppList applications={applications} appWindow={appWindow()}/>
             </box>
         </window>
     )
 }
 
-function onKey(keyVal: number, appWindow: Gtk.Window): void{
+/**
+ * Handles key events for the application launcher
+ * @param keyVal The number of the key pressed 
+ * @param mod Modifier key
+ * @param appWindow The Gtk.Window of the AppLauncher
+ * @param searchEntry The Gtk.Entry of the search entry for the AppList
+ * @param applications The array of applications
+ */
+function onKey(keyVal: number, mod: Gdk.ModifierType, appWindow: Gtk.Window, searchEntry: Gtk.Entry,
+    applications: Array<AstalApps.Application>
+): void{
     if(keyVal == Gdk.KEY_Escape){
         appWindow.visible = false;
         return;
+    }
+    
+    const textStr: string = getKey(keyVal);
+    const currText: string = searchEntry.text;
+
+    // used with backspace
+    const controlUsed: boolean = mod == 4;
+
+    if(textStr.length == 1){
+        searchEntry.set_text(currText + textStr);
+
+    }else if(textStr.includes("backspace")){
+        if(controlUsed){
+            searchEntry.set_text("");
+        }else{
+            if(currText.length != 0){
+                searchEntry.set_text(currText.slice(0, currText.length - 1));
+            }
+        }
+
+    }else if(textStr.includes("return")){
+        if(applications.length != 0){
+            if(applications[0].launch()){
+                appWindow.hide();
+            }
+        }
+    }
+}
+
+function handleClick(x: number, y: number, appBox: Gtk.Box, appWindow: Gtk.Window){
+    const [, rect] = appBox.compute_bounds(appWindow);
+    const pos = new Graphene.Point({x, y});
+
+    if(!rect.contains_point(pos)){
+        appWindow.hide();
     }
 }
 
