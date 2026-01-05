1ff9033c2b81cff35fe2f751cec1a5649b18604c
bobllor <bobllor@yahoo.com>
2026-01-05 17:40:08 -0600

    changes to max height for scrolledwindow, added appWindow Gtk object for turning off launcher, added app launching code, css updates

--- widget/AppLauncher/AppBox/AppList.tsx
+++ widget/AppLauncher/AppBox/AppList.tsx
@@ -2,16 +2,24 @@ import { Gtk } from "ags/gtk4";
 import AstalApps from "gi://AstalApps?version=0.1"
 import { Accessor, For } from "gnim";
 
-export default function AppList({applications}: Props): JSX.Element{
+export default function AppList({applications, appWindow}: Props): JSX.Element{
     return (
         <scrolledwindow
+        maxContentHeight={400}
         propagateNaturalHeight
         propagateNaturalWidth>
             <box
             orientation={Gtk.Orientation.VERTICAL}>
                 <For each={applications}>
                     {(app => (
-                        <button>
+                        <button
+                        class="app-list-entry"
+                        hexpand
+                        onClicked={() => {
+                            if(app.launch()){
+                                appWindow.hide();
+                            };
+                        }}>
                             <box>
                                 <image iconName={app.iconName}/>
                                 <label label={app.name} />
@@ -21,7 +29,9 @@ export default function AppList({applications}: Props): JSX.Element{
                 </For>
                 <box
                 visible={applications(v => v.length == 0)}>
-                    <button>
+                    <button
+                    class="app-list-entry"
+                    hexpand>
                         <box>
                             <label label={"No applications found"} />
                         </box>
@@ -34,4 +44,5 @@ export default function AppList({applications}: Props): JSX.Element{
 
 type Props = {
     applications: Accessor<AstalApps.Application[]>,
+    appWindow: Gtk.Window,
 }
