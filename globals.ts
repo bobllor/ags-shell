b3f178d9162d4abb2502769737fe6bb3070c3804
bobllor <bobllor@yahoo.com>
2026-01-05 01:46:05 -0600

    added globals file

---
+++ globals.ts
@@ -0,0 +1,8 @@
+import Astal from "gi://Astal?version=4.0";
+
+export const anchor = {
+    TOP: Astal.WindowAnchor.TOP,
+    BOT: Astal.WindowAnchor.BOTTOM,
+    RIGHT: Astal.WindowAnchor.RIGHT,
+    LEFT: Astal.WindowAnchor.LEFT,
+}
