a968fc18fa96b2f8c59ff93a6ed093a2730e31e8
bobllor <bobllor@yahoo.com>
2026-01-05 01:46:16 -0600

    updated imports

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 import app from "ags/gtk4/app"
 import style from "./style.scss"
 import Bar from "./widget/Bar"
-import AppLauncher from "./widget/AppLauncher/AppLauncher"
+import AppLauncher from "./widget/AppLauncher"
 
 app.start({
   css: style,
